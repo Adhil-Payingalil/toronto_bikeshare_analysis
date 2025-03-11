16ac16267a4005eceef2e771337c95684001f015
Adhil <adhil1563@gmail.com>
2025-03-11 17:27:33 -0400

    Create creating_station_info_cleaned.sql
    Cleaning station_info by removing station ids with missing names and updating missing capacity with mode.

---
+++ code/data_transformation/creating_station_info_cleaned.sql
@@ -0,0 +1,34 @@
+CREATE OR REPLACE TABLE
+  `toronto-bikeshare-analysis.staging.station_info_cleaned` AS
+WITH
+  ModeCapacity AS (
+    SELECT
+      CAPACITY
+    FROM
+      `toronto-bikeshare-analysis.staging.station_info`
+    WHERE
+      CAPACITY IS NOT NULL
+    GROUP BY
+      CAPACITY
+    ORDER BY
+      COUNT(*) DESC
+    LIMIT 1
+  )
+SELECT
+  station_id,
+  name,
+  lat,
+  lon,
+  IFNULL(
+    si.CAPACITY,
+    (
+      SELECT
+        CAPACITY
+      FROM
+        ModeCapacity
+    )
+  ) AS CAPACITY
+FROM
+  `toronto-bikeshare-analysis.staging.station_info` si
+WHERE
+  name IS NOT NULL;
