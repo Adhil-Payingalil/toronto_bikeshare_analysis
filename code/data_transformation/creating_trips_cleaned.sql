c50f1fae711196c4af726bf423196db6e7f9014c
Adhil <adhil1563@gmail.com>
2025-03-11 17:24:19 -0400

    Create creating_trips_cleaned.sql
    Cleaning trips data for further analysis

---
+++ code/data_transformation/creating_trips_cleaned.sql
@@ -0,0 +1,21 @@
+CREATE OR REPLACE VIEW `toronto-bikeshare-analysis.staging.trips_cleaned` AS
+SELECT
+    Trip_Id,
+    Trip_Duration,
+    Start_Station_Id,
+    start_time_timestamp,
+    DATE(start_time_timestamp) AS start_date,
+    TIME(start_time_timestamp) AS start_time,
+    TIME(TIMESTAMP_TRUNC(start_time_timestamp,HOUR)) AS start_hour,
+    end_station_id,
+    end_time_timestamp,
+    DATE(end_time_timestamp) AS end_date,
+    TIME(end_time_timestamp) AS end_time,
+    User_Type
+FROM
+    `toronto-bikeshare-analysis.staging.staging_trips`
+WHERE
+    Start_Station_Id IN (SELECT station_id FROM `toronto-bikeshare-analysis.staging.station_info_cleaned`)
+    AND End_Station_Id IN (SELECT station_id FROM `toronto-bikeshare-analysis.staging.station_info_cleaned`)
+    AND end_station_id IS NOT NULL
+    AND Trip_Duration !=0;
